34d5d596f3aab42e90dfdced5ccfbab2513359d7
ryota.sakamoto <sakamo.ryota+github@gmail.com>
2018-04-11 01:29:13 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,68 @@
-extern crate hyper;
-extern crate futures;
 extern crate reqwest;
 
 use std::{
     net::{TcpStream, TcpListener, Shutdown},
     io::prelude::*,
 };
-use futures::future::{Future, ok};
-use hyper::{
-    header::{Headers, ContentLength, ContentType},
-    Method,
-    server::{Http, Request, Response, Service}
-};
-
-#[derive(Debug)]
-struct ProxyResponse {
-    method: Method,
-    headers: Headers,
-    body: String,
-}
-impl ProxyResponse {
-    fn new(text: &str, method: Method) -> Self {
-        ProxyResponse {
-            method: method,
-            body: text.to_string(),
-            headers: Headers::new(),
-        }
-    }
-
-    fn parse_response(res: &mut reqwest::Response, method: Method) -> Self {
-        let header = res.headers().clone();
-        ProxyResponse {
-            method: method,
-            body: res.text().unwrap(),
-            headers: header,
-        }
-    }
-}
-struct Server;
-impl Server {
-    fn send_request(&self, req: Request) -> ProxyResponse {
-        let uri = if req.method() == &Method::Connect {
-            format!("https://{}", format!("{}", req.uri()))            
-        } else {
-            format!("{}", req.uri())
-        };
-        let method = req.method().clone();
-            if uri.contains("http://") || uri.contains("https://") {
-            match method {
-                Method::Get => {
-                    let mut res = reqwest::get(&uri).unwrap();
-                    ProxyResponse::parse_response(&mut res, method)
-                },
-                Method::Connect => {
-                    ProxyResponse::new("", method)
-                },
-                _ => {
-                    ProxyResponse::new("", method)
-                }
-            }
-        } else {
-            ProxyResponse::new("you mut use proxy", method)
-        }
-    }
-}
 
 fn main() {
     let server = TcpListener::bind("192.168.3.3:8080").expect("Can not bind");
     for stream in server.incoming() {
         let mut stream = stream.unwrap();
         let mut buf = [0u8; 1024];
-        loop {
-            match stream.read(&mut buf) {
-                Ok(0) => break,
-                Ok(n) => {
-                    match std::str::from_utf8(&buf[0..n]) {
-                        Ok(data) => {
-                            let header = data.split("\r\n").filter(|s| s != &"").collect::<Vec<&str>>();
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    continue;
+                }
+                match std::str::from_utf8(&buf[0..n]) {
+                    Ok(data) => {
+                        let header = data.split("\r\n").filter(|s| s != &"").collect::<Vec<&str>>();
 
-                            let request_header = header[0].split(" ").collect::<Vec<&str>>();
-                            let method = request_header[0];
-                            let mut uri = request_header[1].to_string();
-                            println!("{} {}", method, uri);
-                            if uri.contains(":443") {
-                                uri = format!("https://{}", uri.replace(":443", ""));
+                        let request_header = header[0].split(" ").collect::<Vec<&str>>();
+                        let method = request_header[0];
+                        let mut uri = request_header[1].to_string();
+                        println!("{} {}", method, uri);
+                        if uri.contains(":443") {
+                            uri = format!("https://{}", uri.replace(":443", ""));
+                        }
+                        let uri = &uri;
+                        let protcol = request_header[2];
+
+                        if start_with(uri, "http://") || start_with(uri, "https://") {
+                            match method {
+                                "GET" => {
+                                    let r = reqwest::get(uri).unwrap();
+                                    stream.write(b"HTTP/1.1 200 OK\r\nConnection: Close\r\n\r\n").expect("stream write error");
+                                    stream.shutdown(Shutdown::Both).expect("stream shutdown error");
+                                },
+                                "CONNECT" => {
+                                    stream.write(b"HTTP/1.1 200 Connection Established\r\n\r\n").expect("stream write error");
+
+                                    let mut ssl_stream = TcpStream::connect(request_header[1]).expect("open stream connect error");
+
+                                    let mut a = stream.try_clone().unwrap();
+                                    let mut b = ssl_stream.try_clone().unwrap();
+                                    
+                                    std::thread::spawn(move || {
+                                        std::io::copy(&mut b, &mut a).unwrap();
+                                    });
+                                    std::thread::spawn(move || {
+                                        std::io::copy(&mut stream, &mut ssl_stream).unwrap();
+                                    });
+                                },
+                                m => {
+                                    println!("unknown method: {}", m);
+                                },
                             }
-                            let uri = &uri;
-                            let protcol = request_header[2];
-
-                            if start_with(uri, "http://") || start_with(uri, "https://") {
-                                match method {
-                                    "GET" => {
-                                        let r = reqwest::get(uri).unwrap();
-                                        // println!("{:?}", r);
-                                        stream.write(b"HTTP/1.1 200 OK\r\nConnection: Close\r\n\r\n").expect("stream write error");
-                                        stream.shutdown(Shutdown::Both).expect("stream shutdown error");
-                                    },
-                                    "CONNECT" => {
-                                        stream.write(b"HTTP/1.1 200 Connection Established\r\n\r\n").expect("stream write error");
-
-                                        let mut ssl_stream = TcpStream::connect(request_header[1]).expect("open stream connect error");
-
-                                        let mut buf = [0u8; 1024];
-                                        stream.read(&mut buf).expect("stream read error 2");
-                                        ssl_stream.write(&buf).expect("stream weite error 2");
-
-                                        let mut res = [0u8; 1024];
-                                        ssl_stream.read(&mut res);
-                                        stream.write(&res);
-                                        
-                                        let mut res2 = [0u8; 32 * 1024];
-                                        while let Ok(n) = ssl_stream.read(&mut res2) {
-                                            if n == 0 {
-                                                break;
-                                            }
-                                            stream.write(&res2[..n]);
-                                        }
-
-                                        while let Ok(n) = stream.read(&mut res2) {
-                                            if n == 0 {
-                                                break;
-                                            }
-                                            ssl_stream.write(&res2[..n]);                                            
-                                        }
-                                    },
-                                    m => {
-                                        println!("unknown method: {}", m);
-                                    },
-                                }
-                            } else {
-                                println!("local {:?}", uri);
-                            }
-                        },
-                        _ => break,
-                    }
-                },
-                _ => break,
-            }
+                        } else {
+                            println!("local {:?}", uri);
+                        }
+                    },
+                    _ => {},
+                }
+            },
+            Err(e) => panic!(e),
         }
     }
 }
